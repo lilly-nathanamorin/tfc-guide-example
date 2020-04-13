df227dde867eb14d323c3000081fa7ff460d8354
Nathan Morin <morin_nathan_a@lilly.com>
2020-04-13 10:09:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,51 @@ resource "aws_dynamodb_table" "tfc_example_table" {
     user_name = var.tag_user_name
   }
 }
+
+data "aws_subnet_ids" "private" { 
+  vpc_id = data.aws_cloudformation_export.vpc_id.value
+
+  tags = {
+    "Network" = "Private"
+  }
+}
+
+
+
+resource "aws_db_subnet_group" "default" {
+  subnet_ids = data.aws_subnet_ids.private.ids
+
+  tags = {
+    "service": "datalake_db"
+  }
+}
+
+resource "aws_db_instance" "public-db1" {
+  instance_class      = "db.t3.micro"
+  name                = "test"
+  identifier = "public-db1"
+  engine = "postgres"
+  username = "postgres"
+  password = "initpass"
+  allocated_storage = 10
+  engine_version = "11.5"
+  max_allocated_storage = 10
+  port = 5432
+
+  db_subnet_group_name = aws_db_subnet_group.default.id
+
+  storage_encrypted = true
+
+  allow_major_version_upgrade = true
+  deletion_protection = false
+  skip_final_snapshot = true
+  performance_insights_enabled = true
+
+  lifecycle {
+    ignore_changes = [snapshot_identifier]
+  }
+
+  tags = {
+    "service": "datalake_db"
+  }
+}
